61c1f5f9b3aba8f866c4f1d45572288d900c72df
Baronsmiles <moses2002323@gmail.com>
2023-10-07 20:24:47 -0700

    add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - Checks if a string consists of digits
+ * @str: The input string
+ *
+ * Return: 1 if the string consists of digits, 0 otherwise
+ */
+int check_num(char *str)
+{
+	unsigned int count = 0;
+
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Prints the sum of valid integers passed as arguments
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 if success, 1 if there are non-digit arguments
+ */
+int main(int argc, char *argv[])
+{
+	int count, str_to_int, sum = 0
+		count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
