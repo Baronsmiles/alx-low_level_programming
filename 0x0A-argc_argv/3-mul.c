ae8ec994da337e7da6271e187fc9d01194da5101
Baronsmiles <moses2002323@gmail.com>
2023-10-07 20:14:12 -0700

    mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,51 +1,17 @@
-#include <stdio.h>
-#include "main.h"
-
+#include <stdlib.h>
 /**
- * _atoi - Converts a string to an integer
- * @s: String to be converted
- *
- * Return: The integer converted from the string
+ * main - multipies two numbers
+ * @argc: number of the command line arg...
+ * @argv: the array that contains the command line arg...
+ * Return: (0) when successfull
  */
-int _atoi(char *s)
+int main(int argc, char *argv[])
 {
-	/**
-	 * _atoi - Converts a string to an integer
-	 *
-	 * Description: This function converts a string to an integer.
-	 * It iterates through the input string, considering negative
-	 * signs and stops when a non-digit character is encountered.
-	 *
-	 * @s: String to be converted
-	 *
-	 * Return: The integer converted from the string
-	 */
-
-	int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	if (argc != 3)
 	{
-		if (s[i] == '-')
-			++d;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
+		printf("error\n");
+		return (1);
 	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
